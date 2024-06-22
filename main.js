9ab0b6c72e0dedc9957882084e1b758dc4d13feb
alifpbangsawan <alifbangsawan2@Gmail.com>
2024-06-22 10:47:47 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
-$(document).ready(function() {
-    $(function () {
-    var flame = $("#flame");
-    var txt = $("h1");
-
-    flame.on({
-        click: function () {
-        flame.removeClass("burn").addClass("puff");
-        $(".smoke").each(function () {
-            $(this).addClass("puff-bubble");
-        });
-        $("#glow").remove();
-        txt.hide().html("i wish you happy birthday").delay(750).fadeIn(300);
-        $("#candle").animate(
-            {
-            opacity: ".5"
-            },
-            100
-        );
-        }
-    });
-});});
+document.getElementById('surpriseButton').addEventListener('click', function() {
+    var surpriseDiv = document.getElementById('surprise');
+    surpriseDiv.classList.toggle('hidden');
+});
